a6fd6b7c083f37cdf720a3c6b25a7b398cc72ef2
TRtech00 <97471938+TRtech00@users.noreply.github.com>
2026-07-19 15:42:33 +0300

    Add example configuration

---
+++ sivas-genclerbirligi-v3/config.example.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'db_host' => '127.0.0.1',
+    'db_port' => '3306',
+    'db_name' => 'sivas_genclerbirligi',
+    'db_user' => 'root',
+    'db_pass' => '',
+    'base_url' => 'http://localhost/sivas-genclerbirligi-v3',
+];
